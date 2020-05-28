97ac50fab60ead40fbaa6c66964155f1a6529b1b
Trevor Geene <trevor.geene@gmail.com>
2020-05-27 21:15:35 -0700

    Initial Site Upload

---
+++ index.php
@@ -0,0 +1,229 @@
+<!doctype html>
+<html lang="en-US">
+	<head>
+		<!-- Meta Data -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="author" content="Trevor Geene">
+		<meta name="description" content="A Las Vegas based Web Developer &amp; Consultant with <?php echo date("Y")-2010; ?> years of professional experience building websites, applications, and managing projects.">
+		<meta name="keywords" content="Trevor Geene,Resume,Web Developer,Web Consultant,Las Vegas,AllinWebPro.com,Sin City Gold Buyers,Cyber House Studios,Graphic Intrigue">
+		<meta name="robots" content="index, follow">
+		<meta name="revisit-after" content="1 month">
+		<meta name="msvalidate.01" content="147E3D660A91D75C79ABE39684D338FF" />
+		<meta name="google-site-verification" content="jn7u-0tkgujXLTbfnN0XbIYziJq3k7fKk8u6-oIyhaU" />
+		<meta name="yandex-verification" content="cc15dcdc0623e346" />
+		<!-- Social Meta -->
+		<meta property="fb:app_id" content="1287814887965073" />
+		<meta property="og:url" content="http://trevor.geene.info" />
+		<meta property="og:type" content="website" />
+		<meta property="og:title" content="Trevor Geene - Las Vegas Web Developer &amp; Consultant" />
+		<meta property="og:description" content="A Las Vegas based Web Developer &amp; Consultant with <?php echo date("Y")-2010; ?> years of professional experience building websites, applications, and managing projects." />
+		<meta name="twitter:card" content="summary" />
+		<meta name="twitter:site" content="@trevorgeene" />
+		<meta name="twitter:title" content="Trevor Geene - Las Vegas Web Developer &amp; Consultant" />
+		<meta name="twitter:description" content="A Las Vegas based Web Developer &amp; Consultant with <?php echo date("Y")-2010; ?> years of professional experience building websites, applications, and managing projects." />
+		<!-- Favicons -->
+		<link rel="apple-touch-icon" sizes="57x57" href="favicons/apple-icon-57x57.png">
+		<link rel="apple-touch-icon" sizes="60x60" href="favicons/apple-icon-60x60.png">
+		<link rel="apple-touch-icon" sizes="72x72" href="favicons/apple-icon-72x72.png">
+		<link rel="apple-touch-icon" sizes="76x76" href="favicons/apple-icon-76x76.png">
+		<link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png">
+		<link rel="apple-touch-icon" sizes="120x120" href="favicons/apple-icon-120x120.png">
+		<link rel="apple-touch-icon" sizes="144x144" href="favicons/apple-icon-144x144.png">
+		<link rel="apple-touch-icon" sizes="152x152" href="favicons/apple-icon-152x152.png">
+		<link rel="apple-touch-icon" sizes="180x180" href="favicons/apple-icon-180x180.png">
+		<link rel="icon" type="image/png" sizes="192x192"  href="favicons/android-icon-192x192.png">
+		<link rel="icon" type="image/png" sizes="32x32" href="favicons/favicon-32x32.png">
+		<link rel="icon" type="image/png" sizes="96x96" href="favicons/favicon-96x96.png">
+		<link rel="icon" type="image/png" sizes="16x16" href="favicons/favicon-16x16.png">
+		<link rel="manifest" href="favicons/manifest.json">
+		<meta name="msapplication-TileColor" content="#ffffff">
+		<meta name="msapplication-TileImage" content="favicons/ms-icon-144x144.png">
+		<meta name="theme-color" content="#ffffff">
+		<!-- Styles -->
+		<link href="//fonts.googleapis.com/css?family=Open+Sans:400,400i,700,700i" rel="stylesheet">
+		<link rel="stylesheet" href="css/reset.css">
+		<link rel="stylesheet" href="css/defaults.css">
+		<link rel="stylesheet" href="css/styles.css">
+		<!-- Misc -->
+		<title>Trevor Geene - Las Vegas Web Developer &amp; Consultant</title>
+		<!--[if lt IE 9]>
+			<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+			<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+		<![endif]-->
+	</head>
+	<body>
+		<div id="wrapper">
+			<!-- Page Header -->
+			<header id="contact" itemscope itemtype="http://schema.org/Person">
+				<h1><span itemprop="name">Trevor Geene</span><span class="phone-break"> - </span><span itemprop="jobTitle">Web Developer</span></h1>
+				<p><a title="Phone Number" href="tel:&#43;17024857566" itemprop="telephone" rel="nofollow">702&#46;485&#46;7566</a>
+					- <a title="Email Address" href="mailto:trevor&#46;geene&#64;gmail&#46;com" itemprop="email" rel="nofollow">trevor&#46;geene&#64;gmail&#46;com</a></p>
+			</header>
+			<main id="content">
+				<!-- Profesional Summary -->
+				<section id="summary">
+					<header>
+						<h2>Summary</h2>
+					</header>
+					<p>Web Developer with <?php echo date("Y")-2010; ?> years of professional experience building websites, applications, and managing projects. Excels at creating positive working relationships with a wide variety of clients representing a plethora of industries.</p>
+				</section>
+				<!-- Employment History -->
+				<section id="employment">
+					<header>
+						<h2>Employment</h2>
+					</header>
+					<article itemscope itemtype="http://schema.org/Organization">
+						<header>
+							<h3 itemprop="name">Bid, LLC</h3>
+							<span class="dates">
+								<time datetime="2017-12">December 2017</time> - <time datetime="2020-05">May 2020</time>
+							</span>
+						</header>
+						<h4 itemprop="jobTitle">Lead Web Developer / Project Manager</h4>
+						<ul itemprop="description">
+							<li>Create a tour reseller site using AMP-HTML.</li>
+							<li>Build a content manage system to handle all of the front end systems including:
+								<li>Categories</li>
+								<li>Locations</li>
+								<li>Articles</li>
+								<li>Companies/Venies</li>
+								<li>Products</li>
+								<li>Orders</li>
+								<li>Users</li>
+							<li>
+						</ul>
+					</article>
+					<article itemscope itemtype="http://schema.org/Organization">
+						<header>
+							<h3 itemprop="name">AllinWebPro.com, LLC</h3>
+							<span class="dates">
+								<time datetime="2013-07">July 2013</time> - <time datetime="2017-12">December 2017</time>
+							</span>
+						</header>
+						<h4 itemprop="jobTitle">Senior Web Developer</h4>
+						<ul itemprop="description">
+							<li>Train new web focused team members.</li>
+							<li>Manage clients for both Web Design &amp; Development projects.</li>
+							<li>Work with a team to build websites and webs applications from the ground up.</li>
+							<li>Provide custom development solutions to clients.</li>
+						</ul>
+					</article>
+					<article itemscope itemtype="http://schema.org/Organization">
+						<header>
+							<h3 itemprop="name">Sin City Gold Buyers</h3>
+							<span class="dates">
+								<time datetime="2013-01">January 2013</time> - <time datetime="2014-01">January 2014</time>
+							</span>
+						</header>
+						<h4 itemprop="jobTitle">Web Developer</h4>
+						<ul itemprop="description">
+							<li>Work with owner to build a point of sales and employee management system.</li>
+							<li>Features included: customer tracking, timesheet management and a reporting system.</li>
+						</ul>
+					</article>
+					<article itemscope itemtype="http://schema.org/Organization">
+						<header>
+							<h3 itemprop="name">Cyber House Studios</h3>
+							<span class="dates">
+								<time datetime="2009-11">November 2009</time> - <time datetime="2012-12">December 2012</time>
+							</span>
+						</header>
+						<h4 itemprop="jobTitle">Web Developer</h4>
+						<ul itemprop="description">
+							<li>Work with a team to build websites and webs applications from the ground up.</li>
+							<li>Provide custom development solutions to clients.</li>
+						</ul>
+					</article>
+				</section>
+				<!-- Profesional Skills -->
+				<section id="skills">
+					<header>
+						<h2>Skills</h2>
+					</header>
+					<dl itemscope itemtype="http://schema.org/ItemList">
+						<dt itemprop="name">Front End</dt>
+							<dd>
+								<ul class="skills">
+									<li itemprop="itemListElement">HTML5</li>
+									<li itemprop="itemListElement">AMP-HTML</li>
+									<li itemprop="itemListElement">JavaScript</li>
+									<li itemprop="itemListElement">CSS3</li>
+									<li itemprop="itemListElement">SCSS</li>
+								</ul>
+							</dd>
+						<dt itemprop="name">Backend</dt>
+							<dd>
+								<ul class="skills">
+									<li itemprop="itemListElement">PHP7</li>
+									<li itemprop="itemListElement">PHP5</li>
+									<li itemprop="itemListElement">Python3</li>
+								</ul>
+							</dd>
+						<dt itemprop="name">Data Systems</dt>
+							<dd>
+								<ul class="skills">
+									<li itemprop="itemListElement">JSON</li>
+									<li itemprop="itemListElement">MongoDB</li>
+									<li itemprop="itemListElement">MySQL</li>
+									<li itemprop="itemListElement">XML</li>
+								</ul>
+							</dd>
+						<dt itemprop="name">Frameworks</dt>
+							<dd>
+								<ul class="skills">
+									<li itemprop="itemListElement">CodeIgniter</li>
+									<li itemprop="itemListElement">Flask</li>
+									<li itemprop="itemListElement">jQuery</li>
+									<li itemprop="itemListElement">Meteor</li>
+									<li itemprop="itemListElement">UmbrellaJS</li>
+									<li itemprop="itemListElement">WordPress</li>
+								</ul>
+							</dd>
+						<dt itemprop="name">Other</dt>
+							<dd>
+								<ul class="skills">
+									<li itemprop="itemListElement">Linux</li>
+									<li itemprop="itemListElement">Git</li>
+									<li itemprop="itemListElement">SEO</li>
+									<li itemprop="itemListElement">SVN</li>
+								</ul>
+							</dd>
+					</dl>
+				</section>
+				<!-- Higher Level Education -->
+				<section id="education">
+					<header>
+						<h2>Education</h2>
+					</header>
+					<article itemscope itemtype="http://schema.org/EducationalOrganization">
+						<header>
+							<h3 itemprop="name">The Art Institute of Las Vegas</h3>
+							<span class="dates">
+								<time datetime="2013-03">March 2013</time>
+							</span>
+						</header>
+						<h4 itemprop="name">Web Design &amp; Interactive Media</h4>
+						<ul>
+							<li>Bachelor of Science</li>
+						</ul>
+					</article>
+				</section>
+			</main>
+			<!-- Page Footer -->
+			<footer id="meta">
+				<p>Trevor Geene's Resume &copy; <?php echo date("Y"); ?> | <a href="https://github.com/tgeene" title="GitHub" rel="nofollow">GitHub</a></p>
+			</footer>
+		</div>
+		<!-- Analytics -->
+		<script>
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+		ga('create', 'UA-93164263-1', 'auto');
+		ga('send', 'pageview');
+		</script>
+	</body>
+</html>
